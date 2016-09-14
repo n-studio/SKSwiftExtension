d8f88102629b58b75e0b67f652ada1ea2fc23921
Matthew Nguyen <contact@n-studio.fr>
2016-09-14 08:08:56 +0200

    Updated to Swift 2.3

--- Pod/Classes/UIImage+extension.swift
+++ Pod/Classes/UIImage+extension.swift
@@ -14,12 +14,12 @@ extension UIImage {
         UIGraphicsBeginImageContext(rect.size)
         let context = UIGraphicsGetCurrentContext()
         
-        CGContextSetFillColorWithColor(context, color.CGColor)
-        CGContextFillRect(context, rect)
+        CGContextSetFillColorWithColor(context!, color.CGColor)
+        CGContextFillRect(context!, rect)
         
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        return image
+        return image!
     }
 }
